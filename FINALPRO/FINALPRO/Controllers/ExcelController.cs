73cb0158ab42de2691ec19bac833f662f1e5194e
Monica <Monica@DESKTOP-5HMBFRU>
2020-04-14 02:20:41 -0400

    Parte de Excel creacion y muestra.

---
+++ FINALPRO/FINALPRO/Controllers/ExcelController.cs
@@ -0,0 +1,70 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FINALPRO.Controllers
+{
+    public class ExcelController : Controller
+    {
+        // GET: Excel
+        public ActionResult Subida()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Subida(HttpPostedFileBase Subida)
+        {
+            if (ModelState.IsValid)
+            {
+
+                if (Subida != null && Subida.ContentLength > 0)
+                {
+                    // ExcelDataReader works with the binary Excel file, so it needs a FileStream
+                    // to get started. This is how we avoid dependencies on ACE or Interop:
+                    Stream stream = Subida.InputStream;
+
+                    // We return the interface, so that
+                    IExcelDataReader reader = null;
+
+
+                    if (Subida.FileName.EndsWith(".xls"))
+                    {
+                        reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                    }
+                    else if (Subida.FileName.EndsWith(".xlsx"))
+                    {
+                        reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "El formato de este archivo no es compatible");
+                        return View();
+                    }
+
+                    DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                    {
+                        ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                        {
+                            UseHeaderRow = true
+                        }
+                    });
+                    reader.Close();
+
+                    return View(result.Tables[0]);
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Porfavor cargue un archivo excel");
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ FINALPRO/FINALPRO/Controllers/FiltroContenidoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FINALPRO.Controllers
+{
+    public class FiltroContenidoController : Controller
+    {
+        // GET: FiltroContenido
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
